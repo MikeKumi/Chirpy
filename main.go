cb7fb6ac0cc9179f1aa2af6ef0bd983bacd378b0
MikeKumi <kumicichm@gmail.com>
2024-11-09 22:56:58 -0600

    Added handling for healthz endpoint and minor function documentation

--- main.go
+++ main.go
@@ -1,24 +1,33 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"net/http"
 )
 
+// buildServerMux returns the server mux that will be used to handle the file server and other endpoints
 func buildServeMux() *http.ServeMux {
 	serveMux := http.NewServeMux()
-	serveMux.Handle("/", http.FileServer(http.Dir("web")))
+	serveMux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	serveMux.HandleFunc("/healthz", handleHealthz)
+
 	return serveMux
 }
 
+// Method to be called when /healthz endpoint is hit
+func handleHealthz(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	w.Write([]byte("OK"))
+}
+
+// startServer kicks off the process and logs any errors
 func startServer(serveMux *http.ServeMux, port string) {
 	server := http.Server{Handler: serveMux, Addr: port}
 
-	err := server.ListenAndServe()
+	log.Printf("Serving files on port: %s\n", port)
+	log.Fatal(server.ListenAndServe())
 
-	if err != nil {
-		fmt.Println(err)
-	}
 }
 
 func main() {
